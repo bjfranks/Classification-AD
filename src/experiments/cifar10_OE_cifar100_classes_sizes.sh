81fb7f8b5e6929dacb486220cd5bee3440d58563
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2021-01-13 08:25:17 +0100

    change in classes and size experiment due to time constraint

--- src/experiments/cifar10_OE_cifar100_classes_sizes.sh
+++ src/experiments/cifar10_OE_cifar100_classes_sizes.sh
@@ -4,8 +4,8 @@
 mkdir ../log/cifar10_oe_cifar100;
 
 methods=( hsc bce );
-n_classes=( 1 2 4 8 16 32 64 100 );
-sizes=( 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 );
+n_classes=( 1 2 4 8 16 );
+sizes=( 1 2 4 8 16 32 64 );
 
 for seed in $(seq 1 10);
   do
